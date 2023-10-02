e8527b981d6a50f2b54befb3273180f25b5145cd
Sergii Puliaiev <gorbush@gmail.com>
2023-10-02 18:51:30 +0000

    Adding secondary executions, so it's possible to run the secondary WebApp and Node do work on modifications, while the complete Docker stack is working processing the real data

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -29,7 +29,7 @@ Vue.use(VueJustifiedLayout);
 Vue.config.productionTip = false;
 
 console.log("Configuring axios")
-Vue.prototype.$basePath = window.TIMELINE_BASEPATH ? window.TIMELINE_BASEPATH : process.env.TIMELINE_BASEPATH;
+Vue.prototype.$basePath = (window.TIMELINE_BASEPATH ? window.TIMELINE_BASEPATH : process.env.VUE_APP_TIMELINE_BASEPATH) || "";
 axios.defaults.baseURL = Vue.prototype.$basePath;
 console.log("Configuring axios URL="+axios.defaults.baseURL)
 
